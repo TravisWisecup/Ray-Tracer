04f904d5d7a7938b598cef09b54f7ba1cd6ae9c9
TravisWisecup <60196748+TravisWisecup@users.noreply.github.com>
2021-03-23 09:05:42 -0500

    V3, Ray, and Plane data
    Updates to RayTracer from last Thursday's class

--- RayTester/UnitTest1.cs
+++ RayTester/UnitTest1.cs
@@ -22,9 +22,9 @@ namespace RayTester
         public void Storing_Function()
         {
             var vector3 = new Vector3(3, 4, 5);
-            Assert.AreEqual(3, vector3.x());
-            Assert.AreEqual(4, vector3.y());
-            Assert.AreEqual(5, vector3.z());
+            Assert.AreEqual(3, vector3.x);
+            Assert.AreEqual(4, vector3.y);
+            Assert.AreEqual(5, vector3.z);
         }
         [TestMethod]
         public void Getting_Length()
@@ -37,22 +37,22 @@ namespace RayTester
         {
             var vector3 = new Vector3(0, 0, 1);
             vector3.normalize();
-            Assert.AreEqual(0, vector3.x());
-            Assert.AreEqual(0, vector3.y());
-            Assert.AreEqual(1, vector3.z());
+            Assert.AreEqual(0, vector3.x);
+            Assert.AreEqual(0, vector3.y);
+            Assert.AreEqual(1, vector3.z);
 
             vector3 = new Vector3(1, 1, 0);
             vector3.normalize();
-            Assert.AreEqual((1 / Math.Sqrt(2)), vector3.x());
-            Assert.AreEqual((1 / Math.Sqrt(2)), vector3.y());
-            Assert.AreEqual(0, vector3.z());
+            Assert.AreEqual((1 / Math.Sqrt(2)), vector3.x);
+            Assert.AreEqual((1 / Math.Sqrt(2)), vector3.y);
+            Assert.AreEqual(0, vector3.z);
             Assert.IsTrue(Math.Abs(vector3.length() - 1) < .001);
 
             vector3 = new Vector3(1, 1, 0);
             vector3.normalize(vector3);
-            Assert.AreEqual((1 / Math.Sqrt(2)), vector3.x());
-            Assert.AreEqual((1 / Math.Sqrt(2)), vector3.y());
-            Assert.AreEqual(0, vector3.z());
+            Assert.AreEqual((1 / Math.Sqrt(2)), vector3.x);
+            Assert.AreEqual((1 / Math.Sqrt(2)), vector3.y);
+            Assert.AreEqual(0, vector3.z);
             Assert.IsTrue(Math.Abs(vector3.length() - 1) < .001);
         }
         [TestMethod]
@@ -60,15 +60,15 @@ namespace RayTester
         {
             Vector3 v3 = new Vector3(3, 4, 5);
             v3.scale(.5);
-            Assert.AreEqual(1.5, v3.x());
-            Assert.AreEqual(2, v3.y());
-            Assert.AreEqual(2.5, v3.z());
+            Assert.AreEqual(1.5, v3.x);
+            Assert.AreEqual(2, v3.y);
+            Assert.AreEqual(2.5, v3.z);
 
             v3 = new Vector3(3, 4, 5);
             v3.scale(.5).scale(2);
-            Assert.AreEqual(3, v3.x());
-            Assert.AreEqual(4, v3.y());
-            Assert.AreEqual(5, v3.z());
+            Assert.AreEqual(3, v3.x);
+            Assert.AreEqual(4, v3.y);
+            Assert.AreEqual(5, v3.z);
         }
     }
 }
--- RayTracer/Program.cs
+++ RayTracer/Program.cs
@@ -8,15 +8,14 @@ public class Vector3
 {
     double[] v = new double[3];
 
-    public static Vector3 Zero = new Vector3(0,0,0);
-    public static Vector3 One = new Vector3(1,1,1);
-    public static Vector3 positiveX = new Vector3(1,0,0);
-    public static Vector3 positiveY = new Vector3(0,1,0);
-    public static Vector3 positiveZ = new Vector3(0,0,1);
-
-    int r = 0;
-    int g = 1;
-    int b = 2;
+    public static Vector3 Zero = new Vector3(0, 0, 0);
+    public static Vector3 One = new Vector3(1, 1, 1);
+    public static Vector3 positiveX = new Vector3(1, 0, 0);
+    public static Vector3 positiveY = new Vector3(0, 1, 0);
+    public static Vector3 positiveZ = new Vector3(0, 0, 1);
+    public static Vector3 negativeX = new Vector3(-1, 0, 0);
+    public static Vector3 negativeY = new Vector3(0, -1, 0);
+    public static Vector3 negativeZ = new Vector3(0, 0, -1);
 
     public Vector3(double x, double y, double z)
     {
@@ -24,20 +23,23 @@ public class Vector3
         v[1] = y;
         v[2] = z;
     }
+    private double _x = 0.0;
+    public double x { get { return v[0]; } set { v[0] = value; } }
+    public double y { get { return v[1]; } set { v[1] = value; } }
+    public double z { get { return v[2]; } set { v[2] = value; } }
+    public double r { get { return v[0]; } }
+    public double g { get { return v[1]; } }
+    public double b { get { return v[2]; } }
 
-    public double x()
+
+    public Vector3 clone()
     {
-        return v[0];
+        return new Vector3(this.x, this.y, this.z);
     }
 
-    public double y()
+    public Vector3 clone(double x, double y, double z)
     {
-        return v[1];
-    }
-
-    public double z()
-    {
-        return v[2];
+        return new Vector3(x, y, z);
     }
 
     public double length()
@@ -49,16 +51,7 @@ public class Vector3
     {
         var l = this.length();
 
-        foreach (var i in v)
-        {
-            Console.WriteLine(i);
-        }
-
         v = v.Select(i => i / l).ToArray();
-        foreach(var i in v)
-        {
-            Console.WriteLine(i);
-        }
     }
 
     public void normalize(Vector3 v3)
@@ -75,9 +68,68 @@ public class Vector3
 
     public Vector3 scale(Vector3 v3, double scalar)
     {
-        return new Vector3(v3.x(), v3.y(), v3.z()).scale(scalar);
+        return new Vector3(v3.x, v3.y, v3.z).scale(scalar);
     }
 
+    public Vector3 minus(Vector3 other)
+    {
+        this.x -= other.x;
+        this.y -= other.y;
+        this.z -= other.z;
+
+        return this;
+    }
+
+    public Vector3 minus(Vector3 one, Vector3 two)
+    {
+        Vector3 returnV3 = one.clone().minus(two);
+
+        return returnV3;
+    }
+
+    public Vector3 add(Vector3 other)
+    {
+        this.x += other.x;
+        this.y += other.y;
+        this.z += other.z;
+
+        return this;
+    }
+    
+    public Vector3 add(Vector3 one, Vector3 two)
+    {
+        Vector3 returnV3 = one.clone().add(two);
+        return returnV3;
+    }
+
+    public double dot(Vector3 other)
+    {
+        return this.x * other.x + this.y * other.y + this.z * other.z;
+    }
+
+    public double dot(Vector3 one, Vector3 two)
+    {
+        return one.x * two.x + one.y * two.y + one.z * two.z;
+    }
+
+    public Vector3 cross(Vector3 other)
+    {
+        var _x = this.y*other.z - this.z*other.y;
+        var _y = this.z*other.x - this.x*other.z;
+        var _z = this.x*other.y - this.y*other.x;
+
+        this.x = _x;
+        this.y = _y;
+        this.z = _z;
+
+        return this;
+    }
+
+    public Vector3 cross(Vector3 one, Vector3 two)
+    {
+        Vector3 v3 = one.clone().cross(two);
+        return v3;
+    }
 }
 
 public class Ray
@@ -91,7 +143,57 @@ public class Ray
         this.end = end;
     }
 
+    public Ray normalize()
+    {
+        Vector3 v3 = new Vector3(0,0,0);
+        this.end = v3.add(this.start, this.direction().scale(1/this.length()));
+        return this;
+    }
 
+    public Vector3 direction()
+    {
+        Vector3 v3 = new Vector3(0, 0, 0);
+        return  v3.minus(this.end, this.start);
+    }
+
+    public double length()
+    {
+        return this.direction().length();
+    }
+}
+
+class Plane
+{
+    public double A { get; set; }
+    public double B { get; set; }
+    public double C { get; set; }
+    public double D { get; set; }
+
+    public Plane(double A, double B, double C, double D)
+    {
+        this.A = A;
+        this.B = B;
+        this.C = C;
+        this.D = D;
+    }
+
+    public Plane fromABCD(double A, double B, double C, double D)
+    {
+        return new Plane(A, B, C, D);
+    }
+    public Plane fromABC(double A, double B, double C, Vector3 v3)
+    {
+        double D = -A * v3.x - B * v3.y - C * v3.z;
+        return new Plane(A, B, C, D);
+    }
+
+    public Plane fromThreeVectors(Vector3 v1, Vector3 v2, Vector3 v3)
+    {
+        Vector3 one = v2.minus(v1);
+        Vector3 two = v3.minus(v1);
+        Vector3 normal = v3.cross(one, two);
+        return fromABC(normal.x, normal.y, normal.z, v1);
+    }
 }
 class Program
 {
